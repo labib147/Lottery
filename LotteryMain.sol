bf0501e8c5011f8a2356cb89ebd025b1a0579c47
Nefaur Rahman Labib <nefaurrahmanlabib147@gmail.com>
2022-09-20 14:17:14 +0600

    added ticket map

--- LotteryMain.sol
+++ LotteryMain.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity  ^0.8.0;
 
+/*Errors*/
 error Lottery__SendMoreEth();
 error Lottery__NotOwner();
 
@@ -10,40 +11,33 @@ contract Lottery {
         OPEN,
         CALCULATING
     }
-
+    /*State Variables*/
     address public  owner;
-    address payable[] public players;
-    address payable[] public winners;
+    address[] public players;
+    address[] public winners;
 
-    uint public ticketId;
-    uint public ticketNum;
-    uint public ticketPrice;
-    //uint public entryFee;
-    uint public currentTime = block.timestamp;
-
+    uint[] public ticketId;
+    uint[] public ticketNum; 
+    uint   public ticketPrice;
+    
     // Lottery Variables
     //uint256 private immutable i_entranceFee;
-    
-
-    
-    mapping (uint=> address payable) public lotHis;
-    mapping (uint=> address payable) public winHis;
+        
+    mapping (uint=> address) public lotHis;
+    mapping (uint=> address) public winHis;
+    mapping (address=>uint[]) public ticketsBought;
 
     constructor() {
         owner = msg.sender;
-        //i_entranceFee = ticketFee;
     }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "You are not the owner");
-        // if (msg.sender == owner){
-        //     revert Lottery__NotOwner();
-        // }
         _;
     }
 
-    // function enter() public payable {
-    //     require(msg.value>9 wei, "Not enough funds");
+    // function userTickets(address ad) returns (uint256[]){
+    //     return 
     // }
 
     function setTicketPrice(uint setPrice) public onlyOwner  {
@@ -52,23 +46,25 @@ contract Lottery {
 
     function buyTicket() public payable  {
         require(msg.value == ticketPrice, "Please input correct ticket price");
+        uint256 ticketNumber = block.timestamp;
+        ticketId.push(ticketNumber);
         players.push(payable (msg.sender));
+        ticketsBought[msg.sender].push(ticketNumber);
 
         // if (msg.value > ticketPrice) {
         //     revert Lottery__SendMoreEth(); 
         // }
     }
 
-    // function getCurrentTime() public  returns (uint){ // To get block.timestamp
-    //     return block.timestamp;
-    //     ticketNum  =  currentTime;              
-    // }
+    function getCurrentTime() public view  returns (uint){ // To get block.timestamp
+        return block.timestamp;
+    }
 
     function potBalance() public view returns (uint){
         return address(this).balance; // address(this).balance returns balance of the contract
     }
 
-    function getPlayers() public view returns (address payable[] memory){
+    function getPlayers() public view returns (address[] memory){
         return players;
     }    
 }
